82e99f6c19a05d761870a7876a3decf775813a60
PauliusVizbaraEQ <paulius1207@gmail.com>
2025-10-23 23:29:46 +0300

    wip: game tracker

--- frontend/src/app/(app)/game-tracker/page.tsx
+++ frontend/src/app/(app)/game-tracker/page.tsx
@@ -8,30 +8,35 @@ export default function GameTracker() {
   return (
     <div className="grid grid-rows-[auto_1fr] grid-cols-[1fr_3fr_1fr]">
       <div className="col-span-3 flex justify-between items-center h-12">
-        <div>Turning Point</div>
+        <div className="uppercase font-bold">Turning Point</div>
         <div>
           {[0, 1, 2, 3, 4].map((t) => (
             <button
               key={t}
               onClick={() => setTurn(t)}
-              className={`px-3 py-2 m-2 ${turn === t ? 'bg-blue-500 text-white' : 'bg-gray-200'}`}
+              className={`rounded-sm cursor-pointer px-3 py-2 m-2 font-bold ${turn === t ? 'bg-primary text-white' : 'bg-gray-200'}`}
             >
-              {t}
+              {t == 0 ? 'Setup' : `${t}`}
             </button>
           ))}
+          <button
+            onClick={() => {}}
+            className={`rounded-sm cursor-pointer px-3 py-2 m-2 bg-gray-200 text-black font-bold`}
+          >
+            End game
+          </button>
         </div>
         <div className="flex">
           <h2>Player 1</h2>
-          <h2>
+          <h1>
             | {score.PLAYER1} - {score.PLAYER2} |
-          </h2>
+          </h1>
           <h2>Player 2</h2>
         </div>
       </div>
       <div>2</div>
       <div>3</div>
       <div>4</div>
-      <div>5</div>
     </div>
   )
 }
