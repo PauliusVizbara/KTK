584c00f8953db440e7ebbdc805a7fb254d0b7fc7
Paulius Vizbara <paulius.vizbara@pickitoo.com>
2026-03-27 23:36:39 +0200

    feat: login

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -30,17 +30,17 @@ export default async function RootLayout({children}: {children: React.ReactNode}
       <body>
         <section className="min-h-screen">
           <AuthSessionProvider>
-          {/* The <Toaster> component is responsible for rendering toast notifications used in /app/client-utils.ts and /app/components/DraftModeToast.tsx */}
-          <Toaster />
-          {isDraftMode && (
-            <>
-              <DraftModeToast />
-              {/*  Enable Visual Editing, only to be rendered when Draft Mode is enabled */}
-              <VisualEditing />
-            </>
-          )}
-          {/* The <SanityLive> component is responsible for making all sanityFetch calls in your application live, so should always be rendered. */}
-          <main className="">{children}</main>
+            {/* The <Toaster> component is responsible for rendering toast notifications used in /app/client-utils.ts and /app/components/DraftModeToast.tsx */}
+            <Toaster />
+            {isDraftMode && (
+              <>
+                <DraftModeToast />
+                {/*  Enable Visual Editing, only to be rendered when Draft Mode is enabled */}
+                <VisualEditing />
+              </>
+            )}
+            {/* The <SanityLive> component is responsible for making all sanityFetch calls in your application live, so should always be rendered. */}
+            <main className="">{children}</main>
           </AuthSessionProvider>
         </section>
         <SpeedInsights />
