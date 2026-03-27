74b2886af873c4fe21880dabed8c3d08406959f7
Paulius Vizbara <paulius.vizbara@pickitoo.com>
2026-03-27 05:23:19 +0200

    feat: temp remove dark theme

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -21,10 +21,7 @@ export default async function RootLayout({children}: {children: React.ReactNode}
   const {isEnabled: isDraftMode} = await draftMode()
 
   return (
-    <html
-      lang="en"
-      className="text-zinc-950 antialiased lg:bg-zinc-100 dark:bg-zinc-900 dark:text-white dark:lg:bg-zinc-950"
-    >
+    <html lang="en" className="text-zinc-950 antialiased lg:bg-zinc-100">
       <head>
         <link rel="preconnect" href="https://rsms.me/" />
         <link rel="stylesheet" href="https://rsms.me/inter/inter.css" />
